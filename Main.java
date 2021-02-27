30ed24424d52c8437958e4c8b70d2e21c9aff00b
shivichaubey <36215206+shivichaubey@users.noreply.github.com>
2021-02-27 03:08:28 -0800

    working on the ui
    ma'am ik it's ugly rn it's just getting stuff put on the plane, I got some button functionality so the goal is to get the UI done and then integrate

---
+++ Main.java
@@ -0,0 +1,44 @@
+package active;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	private Stage primaryStage;
+	private BorderPane mainLayout;
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("ACTIVATE");
+		
+		showMainView();
+		showMainItems();
+		
+		
+	}
+	
+	private void showMainItems() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/MainItems.fxml"));
+		BorderPane mainItems = loader.load(); 
+		mainLayout.setCenter(mainItems);
+	}
+	private void showMainView() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
